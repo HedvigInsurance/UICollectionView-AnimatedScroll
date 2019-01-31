f7883e7409f7acc0fde04ce1a5048a6293ee8e85
Sam Pettersson <sam@sampettersson.com>
2019-01-31 22:11:12 +0100

    Add carthage support

---
+++ UICollectionView+AnimatedScroll/UICollectionView_AnimatedScroll.h
@@ -0,0 +1,19 @@
+//
+//  UICollectionView_AnimatedScroll.h
+//  UICollectionView+AnimatedScroll
+//
+//  Created by Sam Pettersson on 2019-01-31.
+//  Copyright © 2019 Sam Pettersson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UICollectionView_AnimatedScroll.
+FOUNDATION_EXPORT double UICollectionView_AnimatedScrollVersionNumber;
+
+//! Project version string for UICollectionView_AnimatedScroll.
+FOUNDATION_EXPORT const unsigned char UICollectionView_AnimatedScrollVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UICollectionView_AnimatedScroll/PublicHeader.h>
+
+
